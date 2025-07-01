5e4606a56ff4e877ead7de19581f385164b5f6fc
BOBBIJDJ <bobbijdj23@outlook.com>
2025-07-01 22:39:40 +0200

    deleted unrelated files

--- assets/map/dungeonDecoration_0.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="dungeonDecoration_0" tilewidth="16" tileheight="16" tilecount="169" columns="13">
- <image source="../../../../Downloads/dungeon_ v1.0/dungeon_ v1.0/dungeonDecoration_0.png" trans="898989" width="208" height="208"/>
-</tileset>
--- assets/map/fire_.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="fire_" tilewidth="16" tileheight="16" tilecount="24" columns="6">
- <image source="../../../../Downloads/dungeon_ v1.0/dungeon_ v1.0/fire_.png" trans="898989" width="96" height="64"/>
-</tileset>
